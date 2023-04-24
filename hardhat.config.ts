a0a8121c69fc62ffadacf9e49c9b71f1e2a46ef3
simon-gridex <developer@gridex.org>
2023-04-24 14:06:25 +0800

    add base goerli

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { HardhatUserConfig } from "hardhat/config";
+import {HardhatUserConfig} from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-solhint";
 import "@nomiclabs/hardhat-etherscan";
@@ -40,7 +40,7 @@ const config: HardhatUserConfig = {
             url: `https://sepolia.infura.io/v3/${process.env.INFURA_KEY}`,
             accounts: [`${process.env.PRIVATE_KEY}`],
         },
-        eth: {
+        mainnet: {
             url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_MAINNET_KEY}`,
             accounts: [`${process.env.PRIVATE_KEY}`],
         },
@@ -56,13 +56,30 @@ const config: HardhatUserConfig = {
             url: `https://arb-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_ARBITRUM_KEY}`,
             accounts: [`${process.env.PRIVATE_KEY}`],
         },
+        "base-goerli": {
+            url: `https://goerli.base.org`,
+            accounts: [`${process.env.PRIVATE_KEY}`],
+        },
     },
     etherscan: {
         apiKey: {
             mainnet: `${process.env.ETHERSCAN_API_KEY}`,
             goerli: `${process.env.ETHERSCAN_API_KEY}`,
-            arbitrumOne: `${process.env.ARBISCAN_API_KEY}`
+            arbitrumOne: `${process.env.ARBISCAN_API_KEY}`,
+            // Basescan doesn't require an API key, however
+            // Hardhat still expects an arbitrary string to be provided.
+            "base-goerli": "PLACEHOLDER_STRING",
         },
+        customChains: [
+            {
+                network: "base-goerli",
+                chainId: 84531,
+                urls: {
+                    apiURL: "https://api-goerli.basescan.org/api",
+                    browserURL: "https://goerli.basescan.org",
+                },
+            },
+        ],
     },
     gasReporter: {},
     contractSizer: {
